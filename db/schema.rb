9bf164904c9618695493d800c053900e4e50e1ed
Wolfgang Barth <wob@swobspace.net>
2017-12-31 13:17:00 +0100

    add pending migration from wobauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908150862) do
+ActiveRecord::Schema.define(version: 20171231121227) do
 
   create_table "wobauth_authorities", force: :cascade do |t|
-    t.integer  "authorizable_id"
-    t.string   "authorizable_type"
-    t.integer  "role_id"
-    t.integer  "authorized_for_id"
-    t.string   "authorized_for_type"
-    t.date     "valid_from"
-    t.date     "valid_until"
+    t.integer "authorizable_id"
+    t.string "authorizable_type"
+    t.integer "role_id"
+    t.integer "authorized_for_id"
+    t.string "authorized_for_type"
+    t.date "valid_from"
+    t.date "valid_until"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["authorizable_id"], name: "index_wobauth_authorities_on_authorizable_id"
@@ -28,16 +28,16 @@ ActiveRecord::Schema.define(version: 20160908150862) do
   end
 
   create_table "wobauth_groups", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
+    t.string "name"
+    t.string "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "wobauth_memberships", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "group_id"
-    t.boolean  "auto",       default: false
+    t.integer "user_id"
+    t.integer "group_id"
+    t.boolean "auto", default: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["group_id"], name: "index_wobauth_memberships_on_group_id"
@@ -45,32 +45,36 @@ ActiveRecord::Schema.define(version: 20160908150862) do
   end
 
   create_table "wobauth_roles", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "wobauth_users", force: :cascade do |t|
-    t.string   "username",               default: "", null: false
-    t.text     "gruppen"
-    t.string   "sn"
-    t.string   "givenname"
-    t.string   "displayname"
-    t.string   "telephone"
-    t.string   "active_directory_guid"
-    t.string   "userprincipalname"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "username", default: "", null: false
+    t.text "gruppen"
+    t.string "sn"
+    t.string "givenname"
+    t.string "displayname"
+    t.string "telephone"
+    t.string "active_directory_guid"
+    t.string "userprincipalname"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "title", default: ""
+    t.string "position", default: ""
+    t.string "department", default: ""
+    t.string "company", default: ""
     t.index ["reset_password_token"], name: "index_wobauth_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_wobauth_users_on_username", unique: true
   end
