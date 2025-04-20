8d5ca501b04f9b05a9b7f42b9639188e6492a46e
Wolfgang Barth <wob@swobspace.net>
2025-04-20 17:32:04 +0200

    add all: to group_link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   include Wobapphelpers::Helpers::All
 
   def group_link(dn)
-    link_to dn, ad_users_path(query: "group:#{dn}"), class: 'primary-link'
+    link_to dn, ad_users_path(query: "all:group:#{dn}"), class: 'primary-link'
   end
 end
